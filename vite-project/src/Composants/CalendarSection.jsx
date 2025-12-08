78e229367fce3357eebbc6bee6a8e76abb5c2934
Lucasa49 <lucasaksu@gmail.com>
2025-12-08 01:30:11 +0100

    fix bug render

--- vite-project/src/Composants/CalendarSection.jsx
+++ vite-project/src/Composants/CalendarSection.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL;
-const API_URL = `${API_BASE_URL}/matchs`;
+const API_URL = `${API_BASE_URL}/api/evenements`;
 
 export default function MatchCalendar() {
   const [matches, setMatches] = useState([]);
